6ee0218f2362048c6454f38e86ae50b4ff3a3f73
Radhika Kulkarni <rrkulkarni@tamu.edu>
2024-10-27 22:44:50 -0500

    start for loop for kmeans algorithm in C++

--- src/kmeans.cpp
+++ src/kmeans.cpp
@@ -2,11 +2,13 @@
 
 // we only include RcppArmadillo.h which pulls Rcpp.h in for us
 #include "RcppArmadillo.h"
+#include <stdlib.h>
 
 // via the depends attribute we tell Rcpp to create hooks for
 // RcppArmadillo so that the build process will know what to do
 //
 // [[Rcpp::depends(RcppArmadillo)]]
+using namespace Rcpp;
 
 // [[Rcpp::export]]
 arma::uvec MyKmeans_c(const arma::mat& X, int K,
@@ -15,15 +17,19 @@ arma::uvec MyKmeans_c(const arma::mat& X, int K,
     
     // Initialize some parameters
     int n = X.n_rows;
-    int p = X.n_cols;
+    //int p = X.n_cols;
     arma::uvec Y(n); // to store cluster assignments
     
     // Initialize any additional parameters if needed
-    
+    arma::uvec oldY(n);
     // For loop with kmeans algorithm
-    
-    
+    for (int iter = 0; iter < numIter; ++iter) {
+      oldY = Y;  // Store previous assignments
+      
+      // Compute distances from each point to each centroid
+      arma::mat distances(n, K, arma::fill::zeros);
+      
     // Returns the vector of cluster assignments
-    return(Y);
+    return(Y+1); //add 1 so we can back to r indexing
 }
 
